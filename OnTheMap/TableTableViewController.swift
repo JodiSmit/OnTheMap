f937bf64080586d1085d30037832443168c18397
Jodi Smit <jodi@nebulahome.com>
2017-04-20 17:19:52 -0700

    Updated Table View Controller

--- OnTheMap/TableTableViewController.swift
+++ OnTheMap/TableTableViewController.swift
@@ -10,10 +10,8 @@ import UIKit
 
 class TableTableViewController: UITableViewController {
 
-    
     let locations = ParseClient.students
 
-    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -27,8 +25,8 @@ class TableTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let studentLocation = locations[indexPath.row]
-        
         let cell = tableView.dequeueReusableCell(withIdentifier: "StudentViewCell") as UITableViewCell!
+        
         cell?.textLabel?.text = "\(studentLocation.firstName) \(studentLocation.lastName)"
         cell?.imageView?.image = UIImage(named: "icon_pin")
         cell?.detailTextLabel?.text = studentLocation.updatedAt
@@ -40,17 +38,16 @@ class TableTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
         let studentLocation = locations[indexPath.row]
-        let app = UIApplication.shared
         
-        if let studUrl = URL(string: studentLocation.linkUrl), app.canOpenURL(studUrl) {
-            app.open(studUrl, options: [:])
+        if let studentUrl = URL(string: studentLocation.linkUrl), UIApplication.shared.canOpenURL(studentUrl) {
+            UIApplication.shared.open(studentUrl, options: [:])
         } else {
             let alertController = UIAlertController()
             
-            alertController.title = "No Valid Url"
-            alertController.message = "The provided URL can't be opened, please select a different student"
+            alertController.title = "Not a Valid URL"
+            alertController.message = "The URL is not available, please select a different student."
             
-            let dismissAction = UIAlertAction(title: "ok", style: UIAlertActionStyle.cancel)
+            let dismissAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.cancel)
             alertController.addAction(dismissAction)
             
             present(alertController, animated: true, completion: nil)
